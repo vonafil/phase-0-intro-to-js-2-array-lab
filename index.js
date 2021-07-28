e32bf4227d01c2775d387468ff420ce6163b461d
Alexei Lifanov <alexeilifanov@alexeis-air.myfiosgateway.com>
2021-07-27 21:33:11 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat(cat) {
+    cats.push(cat)
+}
+
+function destructivelyPrependCat(cat) {
+    cats.unshift(cat)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(cat) {
+    let cats2 = Array.from(cats)
+    cats2.push(cat)
+    return cats2
+}
+
+function prependCat(cat) {
+    let cats2 = Array.from(cats)
+    cats2.unshift(cat)
+    return cats2
+}
+
+function removeLastCat() {
+    let cats2 = Array.from(cats)
+    cats2.pop()
+    return cats2
+}
+
+function removeFirstCat() {
+    let cats2 = Array.from(cats)
+    cats2.shift()
+    return cats2
+}
